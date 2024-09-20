81e057c78f670b94b176cbfa012431a66a3a1200
alx-afk <aliaksandr.hh@gmail.com>
2024-09-20 20:35:29 +0200

    test

--- utils/StaticAnalyse/ErrorFormatter/GithubErrorFormatter.php
+++ utils/StaticAnalyse/ErrorFormatter/GithubErrorFormatter.php
@@ -4,18 +4,30 @@ namespace Utils\StaticAnalyse\ErrorFormatter;
 
 use PHPStan\Command\ErrorFormatter\ErrorFormatter;
 use PHPStan\Command\AnalysisResult;
+use PHPStan\Command\ErrorFormatter\TableErrorFormatter;
 use PHPStan\Command\Output;
 
 final class GithubErrorFormatter implements ErrorFormatter
 {
+    public function __construct(private TableErrorFormatter $tableErrorFormatter)
+    {
+    }
+
     public function formatErrors(AnalysisResult $analysisResult, Output $output) : int
     {
+        if (!getenv('GITHUB_SERVER_URL')) {
+            return $this->tableErrorFormatter->formatErrors($analysisResult, $output);
+        }
+
         $githubRepository = sprintf(
             '%s/%s',
             getenv('GITHUB_SERVER_URL'),
             getenv('GITHUB_REPOSITORY')
         );
-        $branch = str_replace('refs/heads/', '', getenv('GITHUB_REF'));
+        $pathArr = explode('/', $githubRepository);
+        $projectName = $pathArr[array_key_last($pathArr)];
+        $actionDir = sprintf('/home/runner/work/%s/%s/', $projectName, $projectName);
+        $branch = str_replace('refs/heads/', '', getenv('GITHUB_HEAD_REF'));
 
         foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
             $file = $fileSpecificError->getFile();
@@ -25,7 +37,7 @@ final class GithubErrorFormatter implements ErrorFormatter
                 "%s/blob/%s%s#L%d - %s",
                 $githubRepository,
                 $branch,
-                str_starts_with($file, '/app/app/') ? str_replace('/app/app/', '/app/', $file) : $file,
+                str_starts_with($file, $actionDir) ? str_replace($actionDir, '/', $file) : $file,
                 $line,
                 $message
             );
